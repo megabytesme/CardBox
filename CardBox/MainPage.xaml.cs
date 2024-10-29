96bf520e45c6c80e73be9cf66d72e526b9129e97
Ben Hunter <57240557+megabytesme@users.noreply.github.com>
2024-10-29 23:04:19 +0000

    Migrated system specific font and text to code behind file

--- CardBox/MainPage.xaml.cs
+++ CardBox/MainPage.xaml.cs
@@ -10,18 +10,58 @@ namespace CardBox
         {
             InitializeComponent();
             BindingContext = this;
+            SetButtonTextsAndFonts();
 
             // dummy cards
             Cards.Add(new Card { CardTitle = "Loyalty Card 1" });
             Cards.Add(new Card { CardTitle = "Loyalty Card 2" });
         }
 
+        private void SetButtonTextsAndFonts()
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                BackButton.Text = "\uE5C4";
+                BackButton.FontFamily = "sans-serif";
+                HomeButton.Text = "\uE88A";
+                HomeButton.FontFamily = "sans-serif";
+                AddButton.Text = "\uE145";
+                AddButton.FontFamily = "sans-serif";
+                SettingsButton.Text = "\uE8B8";
+                SettingsButton.FontFamily = "sans-serif";
+            }
+            else if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                BackButton.Text = "\uF3A5";
+                BackButton.FontFamily = "system";
+                HomeButton.Text = "\uF6A8";
+                HomeButton.FontFamily = "system";
+                AddButton.Text = "\uF59B";
+                AddButton.FontFamily = "system";
+                SettingsButton.Text = "\uF751";
+                SettingsButton.FontFamily = "system";
+            }
+            else if (DeviceInfo.Platform == DevicePlatform.WinUI)
+            {
+                BackButton.Text = "\uE72B";
+                BackButton.FontFamily = "Segoe MDL2 Assets";
+                HomeButton.Text = "\uE80F";
+                HomeButton.FontFamily = "Segoe MDL2 Assets";
+                AddButton.Text = "\uE710";
+                AddButton.FontFamily = "Segoe MDL2 Assets";
+                SettingsButton.Text = "\uE713";
+                SettingsButton.FontFamily = "Segoe MDL2 Assets";
+            }
+        }
+
         private void OnBackClicked(object sender, EventArgs e)
         {
+
         }
 
         private void OnHomeClicked(object sender, EventArgs e)
         {
+
         }
 
         private void OnAddClicked(object sender, EventArgs e)
@@ -31,6 +71,7 @@ namespace CardBox
 
         private void OnSettingsClicked(object sender, EventArgs e)
         {
+
         }
     }
 
@@ -38,5 +79,4 @@ namespace CardBox
     {
         public string CardTitle { get; set; } = string.Empty;
     }
-
 }
