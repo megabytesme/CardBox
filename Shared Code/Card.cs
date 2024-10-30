f0c885305d0fd139b62f0ea81f1c945b77c87e7f
Ben Hunter <57240557+megabytesme@users.noreply.github.com>
2024-10-30 09:38:21 +0000

    Added Card class

---
+++ Shared Code/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared_Code
+{
+    public class Card
+    {
+        public string CardName { get; set; } = string.Empty;
+        public int CardNumber { get; set; } = int.MaxValue;
+        public enum DisplayType { get; set; };
+    }
+
+    public enum DisplayType
+    {
+        BarCode,
+        QR
+    }
+}
