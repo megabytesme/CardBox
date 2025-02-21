5c349b2c5ad3b6ba2016ad66d7e84d141efa6078
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-02-21 22:17:39 +0000

    Set default page on launch to the homepage

--- 1709 UWP/MainPage.xaml.cs
+++ 1709 UWP/MainPage.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 using Shared_Code;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace _1709_UWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public ObservableCollection<Card> Cards => CardRepository.Instance.Cards;
@@ -19,16 +13,16 @@ namespace _1709_UWP
         {
             this.InitializeComponent();
             DataContext = this;
+
+            // Navigate to HomePage by default
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(HomePage));
         }
 
         public Frame MainContentFrame => ContentFrame;
-
-        private void ViewCard_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.CommandParameter is Card selectedCard)
-            {
-                ContentFrame.Navigate(typeof(CardDetailPage), selectedCard);
-            }
-        }
     }
 }
