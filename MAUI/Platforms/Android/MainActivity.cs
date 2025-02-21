4ac28b2137854b053b80fc3233e0150b4ad66334
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-02-21 02:54:59 +0000

    Rename from CardBox to MAUI

--- MAUI/Platforms/Android/MainActivity.cs
+++ MAUI/Platforms/Android/MainActivity.cs
@@ -1,11 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using AndroidX.Core.Content;
+using Color = Android.Graphics.Color;
 
 namespace CardBox
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var accentColor = new Color(ContextCompat.GetColor(this, Resource.Color.accent_material_light));
+
+            Window.SetStatusBarColor(accentColor);
+        }
     }
 }
