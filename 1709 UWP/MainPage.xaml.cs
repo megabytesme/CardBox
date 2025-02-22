d5ebc25f0486fbfa3cccbcdc30908a297c7140eb
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-02-22 13:22:57 +0000

    Replaced custom navbar control with native NavigationView

--- 1709 UWP/MainPage.xaml.cs
+++ 1709 UWP/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Shared_Code;
+using Windows.UI;
+using Windows.UI.Core;
 
 namespace _1709_UWP
 {
@@ -14,13 +17,82 @@ namespace _1709_UWP
             this.InitializeComponent();
             DataContext = this;
 
-            // Navigate to HomePage by default
+            ApplyAcrylic();
+
+            NavView.ItemInvoked += NavView_ItemInvoked;
             Loaded += MainPage_Loaded;
+            ContentFrame.Navigated += ContentFrame_Navigated;
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+        }
+
+        private void ApplyAcrylic()
+        {
+            this.Background = new AcrylicBrush
+            {
+                BackgroundSource = AcrylicBackgroundSource.HostBackdrop,
+                TintColor = Colors.Transparent,
+                TintOpacity = 0.6,
+                FallbackColor = Colors.Gray
+            };
         }
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             ContentFrame.Navigate(typeof(HomePage));
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                var item = args.InvokedItemContainer as NavigationViewItem;
+                switch (item.Tag.ToString())
+                {
+                    case "HomePage":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "AddCardPage":
+                        ContentFrame.Navigate(typeof(AddCardPage));
+                        break;
+                }
+            }
+        }
+
+        private void ContentFrame_Navigated(object sender, Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            var backButton = (Button)FindName("BackButton");
+            if (backButton != null)
+            {
+                backButton.Visibility = ContentFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+             SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = ContentFrame.CanGoBack ?
+                 AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void MainPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                e.Handled = true;
+                GoBack();
+            }
+        }
+
+        private void GoBack()
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                ContentFrame.GoBack();
+            }
         }
 
         public Frame MainContentFrame => ContentFrame;
