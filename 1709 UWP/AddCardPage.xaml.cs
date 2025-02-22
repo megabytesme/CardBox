db94d21d144e4b3fd2e02884e083284a3b6e9e04
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-02-22 01:03:10 +0000

    Added ability to scan card number and card type

--- 1709 UWP/AddCardPage.xaml.cs
+++ 1709 UWP/AddCardPage.xaml.cs
@@ -2,6 +2,11 @@
 using Windows.UI.Xaml.Controls;
 using Shared_Code;
 using System;
+using ZXing;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace _1709_UWP
 {
@@ -54,6 +59,46 @@ namespace _1709_UWP
             CardRepository.Instance.AddCard(newCard);
 
             Frame.GoBack();
+        }
+
+        private async void OnScanCard(object sender, RoutedEventArgs e)
+        {
+            var cameraCapture = new CameraCaptureUI();
+            cameraCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            cameraCapture.PhotoSettings.CroppedSizeInPixels = new Windows.Foundation.Size(200, 200);
+
+            var photo = await cameraCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (photo != null)
+            {
+                var stream = await photo.OpenAsync(FileAccessMode.Read);
+                var bitmapDecoder = await BitmapDecoder.CreateAsync(stream);
+                var bitmap = await bitmapDecoder.GetSoftwareBitmapAsync();
+
+                var barcodeReader = new BarcodeReader();
+                var result = barcodeReader.Decode(bitmap);
+
+                if (result != null)
+                {
+                    cardNumberEntry.Text = result.Text;
+                    if (result.BarcodeFormat == BarcodeFormat.QR_CODE)
+                    {
+                        displayPicker.SelectedItem = displayPicker.Items[0];
+                    }
+                    else if (result.BarcodeFormat == BarcodeFormat.CODE_128)
+                    {
+                        displayPicker.SelectedItem = displayPicker.Items[1];
+                    }
+                }
+                else
+                {
+                    await new ContentDialog
+                    {
+                        Title = "Error",
+                        Content = "Failed to read barcode.",
+                        CloseButtonText = "OK"
+                    }.ShowAsync();
+                }
+            }
         }
     }
 }
