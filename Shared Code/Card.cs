74ae2b60ebbf68d7f6d38c4dad9e34c3bd8a3b45
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-02-23 15:45:22 +0000

    Use ZXing across the codebase for barcode format

--- Shared Code/Card.cs
+++ Shared Code/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using SQLite;
+using ZXing;
 
 namespace Shared_Code
 {
@@ -9,7 +10,7 @@ namespace Shared_Code
         private string _cardName;
         private string _cardNickname;
         private string _cardNumber;
-        private DisplayType _displayType;
+        private BarcodeFormat _displayType;
 
         [PrimaryKey, AutoIncrement]
         public long Id
@@ -64,7 +65,7 @@ namespace Shared_Code
             }
         }
 
-        public DisplayType DisplayType
+        public BarcodeFormat DisplayType
         {
             get => _displayType;
             set
@@ -83,94 +84,5 @@ namespace Shared_Code
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-    }
-
-    public enum DisplayType
-    {
-        // (Only the enum) is based on the ZXing.NET.MAUI package
-        //
-        // Summary:
-        //     Aztec 2D barcode format.
-        Aztec = 0,
-        //
-        // Summary:
-        //     CODABAR 1D format.
-        Codabar = 1,
-        //
-        // Summary:
-        //     Code 39 1D format.
-        Code39 = 2,
-        //
-        // Summary:
-        //     Code 93 1D format.
-        Code93 = 3,
-        //
-        // Summary:
-        //     Code 128 1D format.
-        Code128 = 4,
-        //
-        // Summary:
-        //     Data Matrix 2D barcode format.
-        DataMatrix = 5,
-        //
-        // Summary:
-        //     EAN-8 1D format.
-        Ean8 = 6,
-        //
-        // Summary:
-        //     EAN-13 1D format.
-        Ean13 = 7,
-        //
-        // Summary:
-        //     ITF (Interleaved Two of Five) 1D format.
-        Itf = 8,
-        //
-        // Summary:
-        //     MaxiCode 2D barcode format.
-        MaxiCode = 9,
-        //
-        // Summary:
-        //     PDF417 format.
-        Pdf417 = 10,
-        //
-        // Summary:
-        //     QR Code 2D barcode format.
-        QrCode = 11,
-        //
-        // Summary:
-        //     RSS 14
-        Rss14 = 12,
-        //
-        // Summary:
-        //     RSS EXPANDED
-        RssExpanded = 13,
-        //
-        // Summary:
-        //     UPC-A 1D format.
-        UpcA = 14,
-        //
-        // Summary:
-        //     UPC-E 1D format.
-        UpcE = 15,
-        //
-        // Summary:
-        //     UPC/EAN extension format. Not a stand-alone format.
-        UpcEanExtension = 16,
-        //
-        // Summary:
-        //     MSI
-        Msi = 17,
-        //
-        // Summary:
-        //     Plessey
-        Plessey = 18,
-        //
-        // Summary:
-        //     Intelligent Mail barcode
-        Imb = 19,
-        //
-        // Summary:
-        //     Pharmacode format.
-        PharmaCode = 20
     }
 }
